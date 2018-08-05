cf4157ae8caf5f2720576980485e7e9865823a7d
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-08-05 16:55:26 -0300

    Added movie cell and extension to download image

---
+++ MoviesApp/Cell/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MoviesApp
+//
+//  Created by Rodrigo Vianna on 05/08/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleMovieLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    func configure(movie: Movie) {
+        titleMovieLabel.text = movie.title
+        releaseDateLabel.text = movie.date
+        posterImageView.downloadImage(from: movie.poster)
+    }
+    
+}
