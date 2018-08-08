370abd05f2854bcac9b82e7d7aee735bb4ca0222
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-08-08 09:29:53 -0300

    Refactor on json logic

--- MoviesApp/Model/Movie.swift
+++ MoviesApp/Model/Movie.swift
@@ -23,6 +23,6 @@ class Movie {
     init(withJSON json: JSON) {
         self.title = json["title"].stringValue
         self.date = json["release_date"].stringValue
-        self.poster = "https://image.tmdb.org/t/p/w185\(json["poster_path"].stringValue)"
+        self.poster = json["poster_path"].stringValue
     }
 }
