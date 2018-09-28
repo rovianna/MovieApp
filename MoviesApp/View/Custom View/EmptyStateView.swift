ad22e0dda316d1a213f8483ecc45d2d6199d2c4c
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-09-28 14:29:18 -0300

    Added an empty state view

--- MoviesApp/View/Custom View/EmptyStateView.swift
+++ MoviesApp/View/Custom View/EmptyStateView.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2018 Rodrigo Vianna. All rights reserved.
 //
 
-import Foundation
+protocol EmptyStateViewDelegate {
+    func emptyStateView(_ emptyStateView: EmptyStateView, didTap refreshAction: UIButton)
+}
+
+import UIKit
+
+class EmptyStateView: UIView {
+    class var instance: EmptyStateView {
+        let nib = UINib(nibName: "EmptyStateView", bundle: nil)
+        let view = nib.instantiate(withOwner: nil, options: nil).first as! EmptyStateView
+        
+        return view
+    }
+    
+    var delegate: EmptyStateViewDelegate?
+    
+    @IBOutlet weak var errorDescriptionLabel: UILabel!
+    
+    @IBAction func retryAction(_ sender: UIButton) {
+        delegate?.emptyStateView(self, didTap: sender)
+    }
+    
+    func configureView(error: State) {
+        switch error {
+        case .empty:
+            errorDescriptionLabel.text = "Nenhum filme foi encontrado :("
+        default:
+            errorDescriptionLabel.text = "Oh não! Houve um erro ao buscar seus filmes :("
+        }
+    }
+    
+}
